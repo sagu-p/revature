9f9ebf06554532795cbbc5953debb0a64e9504d1
Sagar Patel <sagarpatel.sp2611@gmail.com>
2021-01-03 19:47:59 -0500

    commit on 1-3-2021 7:47 PM EST.

--- Week 2/date_time_demo/src/eg1/DateValidation.java
+++ Week 2/date_time_demo/src/eg1/DateValidation.java
@@ -6,7 +6,7 @@ import java.util.Date;
 
 public class DateValidation {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		
 		String s = "11-26-1998";
 		if(s.matches("[0-9]{2}-[0-9]{2}-[0-9]{4}")) {
